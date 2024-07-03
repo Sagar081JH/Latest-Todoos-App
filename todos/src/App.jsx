fc388c4397767914431b717f151876325dadbfb4
Sagar081JH <sghumare11@gmail.com>
2024-07-03 12:25:32 +0530

    initial commit

---
+++ todos/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Todos from "./Components/Todos";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Todos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
